e07d1d605ee81d826d106091ceffd6f05d5f4a7b
Bruno Magalhães <brunonoroes@gmail.com>
2025-11-07 12:47:49 -0300

    Initial Commit

---
+++ cupomgpt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cupomgpt
+//
+//  Created by Bruno Magalhães on 07/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cupomgpt/cupomgptApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupomgptApp.swift
+//  cupomgpt
+//
+//  Created by Bruno Magalhães on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct cupomgptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
